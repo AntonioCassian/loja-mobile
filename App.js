9bc3299018fd76f4e7430948484b38afb68fa415
Antônio Martins <92797981+AntonioCassian@users.noreply.github.com>
2024-03-26 14:55:49 -0300

    feat: create prduct card

--- App.js
+++ App.js
@@ -4,12 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <>
-    <StatusBar style="auto" />
+    <StatusBar style="light" />
     <View style={styles.header}>
       <View></View>
       <Text style={styles.title}>Produtos</Text>
     </View>
+
       <View style={styles.container}>
+    <View style={styles.card}>
+      <View style={styles.icone}></View>
+      <View style={styles.imag}></View>
+      <Text style={styles.name}>Nike Air Max 200</Text>
+      <Text style={styles.valor}>R$ 26,00</Text>
+    </View>
         <Text>Hello World!😁😉</Text>
       </View>
     </>
@@ -35,5 +42,26 @@ const styles = StyleSheet.create({
     fontWeight:'900',
     color: '#fff',
     marginTop: 70
+  },
+  card: {
+    //height: '50%',
+    backgroundColor: '#FFF3F3',
+    //width: '40%',
+    paddingVertical: 27,
+    paddingHorizontal: 27,
+    borderRadius: 10
+  },
+  imag:{
+    backgroundColor: 'blue',
+    height: 120,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: '900',
+    marginVertical: 14
+  },
+  valor: {
+    fontWeight: 12,
+    fontWeight: 'bold'
   }
 });
